8f6062b4a0a38acc3f2fec7a0c25a4cec2de0e2b
carslan3455 <carslan3455@gmail.com>
2021-01-18 16:53:47 +0100

    Add project files.

---
+++ BankaKonto/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankaKonto
+{
+    class MusteriManager
+{
+    public void Ekle(Musteri musteri)
+    {
+        Console.WriteLine(musteri.Ad + " isimli musteri icin hesap olusturulmustur...");
+    }
+
+    public void Listele(Musteri[] musteriler)
+    {
+        foreach (Musteri musteri in musteriler)
+        {
+            Console.WriteLine(musteri.Ad);
+        }
+        Console.WriteLine("Musteriler Listelendi...");
+    }
+
+    public Musteri[] Sil(Musteri[] musteriler,Musteri musteri)
+    {
+       Console.WriteLine(musteri.Ad + " isimli musteriye ait hesap silinmistir...");
+
+        return musteriler;
+    }
+}
+
+}
---
+++ BankaKonto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BankaKonto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+    Musteri musteri1 = new Musteri();
+    musteri1.Ad = "Ihsan";
+    musteri1.Soyad = "A";
+
+    Musteri musteri2 = new Musteri();
+    musteri2.Ad = "Sinem";
+    musteri2.Soyad = "A";
+
+
+    Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+    MusteriManager musteriManager = new MusteriManager();
+
+    musteriManager.Ekle(musteri1);
+    musteriManager.Ekle(musteri2);
+
+    musteriManager.Listele(musteriler);
+
+    musteriManager.Sil(musteriler,musteri1);
+
+    musteriManager.Listele(musteriler);
+
+        }
+    }
+}
